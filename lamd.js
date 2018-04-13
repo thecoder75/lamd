ca7eaa35409a625120b0cdf69f77fd24b0621b2c
TheCoder75 <thecoder1975@gmail.com>
2018-04-13 07:49:58 -0400

    See changelog for details.

--- lamd.js
+++ lamd.js
@@ -6,10 +6,13 @@ const   os = require('os'),
         fs = require('fs'),
         http = require('http'),
         LiveMe = require('liveme-api'),
-        ffmpeg = require('fluent-ffmpeg');
+        ffmpeg = require('fluent-ffmpeg'),
+        m3u8stream = require('./modules/m3u8stream'); 
 
 var     config = {
+            downloaderFFMPEG: true,
             downloadPath: os.homedir() + '/Downloads',
+            downloadChunks: 10,
             downloadTemplate: '%%replayid%%',
             loopCycle: 30,
             localPort: 8280,
@@ -41,19 +44,47 @@ function main() {
         fs.readFile('config.json', 'utf8', (err,data) => {
             if (!err) {
                 config = JSON.parse(data);
-                if (config.loopCycle > 120) config.loopCycle = 120;
+
+                // These options only come int play when using the stream downloader and not FFMPEG
+                if ((config.downloaderFFMPEG == undefined) || (config.downloaderFFMPEG == null)) config.downloaderFFMPEG = true;
+                if (config.downloadChunks < 2) config.downloadChunks = 2;
+                if (config.downloadChunks > 250) config.downloadChunks = 250;
+
+                if (config.loopCycle > 360) config.loopCycle = 360;
                 if (config.loopCycle < 15) config.loopCycle = 15;
+
                 if ((config.console_output == undefined) || (config.console_output == null)) config.console_output = false;
             }
+
+
+            if (config.console_output) {
+                process.stdout.write("\x1b[1;34mLiveMe Account Monitor Daemon (LAMD)\n\x1b[0;34mhttps://thecoderstoolbox.com/lamd\n");
+                process.stdout.write("\x1b[1;30m------------------------------------------------------------------------------\n");
+                process.stdout.write("\x1b[1;32m     Scan Interval:      \x1b[1;36m" + config.loopCycle + " \x1b[1;32mminutes\n\n");
+
+                process.stdout.write("\x1b[1;32m     Download Path:      \x1b[1;36m" + config.downloadPath + "\n");
+                process.stdout.write("\x1b[1;32m     Download Template:  \x1b[1;36m" + config.downloadTemplate + "\n\n");
+                process.stdout.write("\x1b[1;32m     Download Engine:    \x1b[1;36m" + (config.downloaderFFMPEG ? 'FFMPEG' : 'Stream Downloader') + "\n");
+                if (config.downloaderFFMPEG == false) {
+                    process.stdout.write("\x1b[1;32m     Download Chunks:    \x1b[1;36m" + config.downloadChunks + "\x1b[1;32m at a time\n");    
+                }
+                process.stdout.write("\x1b[1;30m------------------------------------------------------------------------------\n");
+                process.stdout.write("\x1b[0;37m");
+            }
+
+
         });
     }
 
-    if (config.console_output) {
-        process.stdout.write("LiveMe Account Monitor Daemon (LAMD)\nhttps://thecoderstoolbox.com/lamd\n\n");
+    for (var i = 0; i < process.argv.length; i++) {
+        if (process.argv[i] == '--writecfg') {
+            fs.writeFile(
+                'config.json', 
+                JSON.stringify(config, null, 2), 
+                () => {}
+            );            
+        }
     }
-
-
-
 
     /*
             Load Account List
@@ -64,7 +95,7 @@ function main() {
                 accounts = JSON.parse(data);
 
                 if (config.console_output) {
-                    process.stdout.write(accounts.length + " accounts loaded in.\n\n");
+                    process.stdout.write("\x1b[1;33m" + accounts.length + " \x1b[1;34maccounts loaded in.\n");
                 }
 
             }
@@ -76,17 +107,17 @@ function main() {
             if (!err) {
                 download_list = JSON.parse(data);
 
-                if (config.console_output) {
-                    process.stdout.write("Download queue file found, importing it.\n\n");
+                if (download_list.length > 0) {
+                    if (config.console_output) process.stdout.write("\x1b[1;33mResuming existing download queue...\n");
+                
+                    setTimeout(() => {
+                        downloadFile();
+                    }, 5000);
                 }
-
-                setTimeout(() => {
-                    downloadFile();
-                }, 1000);
-
             }
         });
     }
+
 
 
     /*
@@ -98,16 +129,15 @@ function main() {
             minuteTick = 0;
             setImmediate(() => { 
                 account_index = 0;
-                if (config.console_output) process.stdout.write("Beginning account scan for new replays.\n");
                 accountScanLoop();
             });
         }
     }, 60000);
+
     setTimeout(() => {
         account_index = 0;
-        if (config.console_output) process.stdout.write("Beginning account scan for new replays.\n");
         accountScanLoop();
-    }, 2000);
+    }, 5
 
     
     /*
@@ -148,11 +178,11 @@ function main() {
 
                     response.message = 'Account added.';
                     response.code = 200;
-                    if (config.console_output) process.stdout.write("Added " + chunks[1] + " for monitoring.\n");
+                    if (config.console_output) process.stdout.write("\x1b1;36mAdded \x1b[1;33m" + chunks[1] + " \x1b[1;36mfor monitoring.\n");
                 } else {
                     response.message = 'Account already in list.';
                     response.code = 302;
-                    if (config.console_output) process.stdout.write("Account " + chunks[1] + " already in database.\n");
+                    if (config.console_output) process.stdout.write("\x1b[1;31mAccount \x1b[1;33m" + chunks[1] + " \x1b[1;31malready in database.\n");
                 }
                 break;
 
@@ -181,7 +211,7 @@ function main() {
                         accounts.splice(i, 1);
                         response.message = 'Account removed.';
                         response.code = 200;
-                        if (config.console_output) process.stdout.write("Account " + chunks[1] + " removed from list.\n");
+                        if (config.console_output) process.stdout.write("\x1b[1;36mAccount \x1b[1;33m" + chunks[1] + " \x1b[1;36mremoved from list.\n");
                     }
                 }
 
@@ -211,8 +241,9 @@ function main() {
                 response.data = [];
                 var isnum = /^\d+$/.test(chunks[1]);
                 if (isnum) {
-                    if (config.console_output) process.stdout.write("Replay " + chunks[1] + " added to download queue.\n");
+                    if (config.console_output) process.stdout.write("\x1b[1;36mReplay \x1b[1;33m" + chunks[1] + " \x1b[1;36m- added to queue.  \r");
                     download_list.push(chunks[1]);
+                    downloadFile();
                 }
                 break;
 
@@ -224,7 +255,7 @@ function main() {
 
 
             case 'shutdown':
-                if (config.console_output) process.stdout.write("Shutting down and storing information...\n");
+                if (config.console_output) process.stdout.write("\x1b[1;31mShutting down and storing information...\n");
 
                 setTimeout(() => {
                     process.exit(0);    
@@ -261,12 +292,12 @@ function accountScanLoop() {
     if (account_index < accounts.length) {
         setTimeout(() => {
             accountScanLoop();
-        }, 200);
+        }, 250);
     }
 
-    setTimeout(function(){
+    setImmediate(function(){
         if (account_index < accounts.length) { account_index++; scanForNewReplays(account_index); }
-    }, 50);
+    });
 }
 
 /*
@@ -276,7 +307,7 @@ function scanForNewReplays(i) {
 
     if (accounts[i] == undefined) return;
 
-    LiveMe.getUserReplays(accounts[i].userid, 1, 5).then(replays => {
+    LiveMe.getUserReplays(accounts[i].userid, 1, 10).then(replays => {
 
         if (replays == undefined) return;
         if (replays.length < 1) return;
@@ -287,7 +318,7 @@ function scanForNewReplays(i) {
             last_scanned = 0,
             dt = Math.floor((new Date()).getTime() / 1000);
 
-        last_scanned = accounts[i].scanned - 86400;
+        last_scanned = accounts[i].scanned;
         accounts[i].scanned = dt;
         
         fs.writeFile(
@@ -297,7 +328,7 @@ function scanForNewReplays(i) {
         );
         
 
-        if (config.console_output) process.stdout.write("UserID: " + userid + "\n");
+        var replay_count = 0;
         for (ii = 0; ii < replays.length; ii++) {
 
             // If we take the video time and subtract the last time we scanned and its
@@ -309,7 +340,7 @@ function scanForNewReplays(i) {
                     if (download_list[j] == replays[ii].vid) add_replay = false;
                 }
                 if (add_replay == true) {
-                    if (config.console_output) process.stdout.write("\t" + replays[ii].vid + " added to queue.\n");
+                    replay_count++;
                     download_list.push(replays[ii].vid);
                     fs.writeFile(
                         'queued.json', 
@@ -318,13 +349,15 @@ function scanForNewReplays(i) {
                             // Queue file was written
                         }
                     );
-                    setTimeout(()=>{
-                        downloadFile();
-                    }, 1000);
-                } else
-                    if (config.console_output) process.stdout.write("\t" + replays[ii].vid + " already in queue.\n");
-
+                }
             }
+        }
+
+        if (replay_count > 0) {
+            if (config.console_output) process.stdout.write("\x1b[1;36mAdding \x1b[1;33m"+replay_count+" \x1b[1;36mreplays for \x1b[1;33m"+userid+"        \n");
+            downloadFile();
+        } else {
+            if (config.console_output) process.stdout.write("\x1b[1;36mNo new replays found for \x1b[1;33m"+userid+"\x1b[1;36m.                            \n");
         }
 
     });
@@ -348,16 +381,6 @@ function downloadFile() {
     if (downloadActive == true) return;
     if (download_list.length == 0) return;
 
-
-    // Update current queue file
-    fs.writeFile(
-        'queued.json', 
-        JSON.stringify(download_list), 
-        () => {
-            // Queue file was written
-        }
-    );
-
     LiveMe.getVideoInfo(download_list[0]).then(video => {
 
         var filename = config.downloadTemplate
@@ -375,57 +398,138 @@ function downloadFile() {
         filename = filename.replace(/([^a-z0-9\s]+)/gi, '-');
         filename = filename.replace(/[\u{0080}-\u{FFFF}]/gu, '');
 
-        filename += '.mp4';
-        
-        ffmpeg(video.hlsvideosource)
-            .outputOptions([
-                '-c copy',
-                '-bsf:a aac_adtstoasc',
-                '-vsync 2',
-                '-movflags faststart'
-            ])
-            .output(config.downloadPath + '/' + filename)
-            .on('end', function(stdout, stderr) {
-                if (config.console_output) process.stdout.write(download_list[0] + " - downloaded successfully. \n");
+        if (config.downloaderFFMPEG == true) { 
 
-                download_list.shift();
-                download_active = false;
-                setTimeout(() => {
+            filename += '.mp4';
+
+            ffmpeg(video.hlsvideosource)
+                .outputOptions([
+                    '-c copy',
+                    '-bsf:a aac_adtstoasc',
+                    '-vsync 2',
+                    '-movflags faststart'
+                ])
+                .output(config.downloadPath + '/' + filename)
+                .on('end', function(stdout, stderr) {
+                    if (config.console_output) process.stdout.write("\x1b[1;34mReplay \x1b[1;33m" + download_list[0] + " \x1b[1;34m- downloaded.                   \n");
+
+                    download_list.shift();
+                    downloadActive = false;
+
+                    // Update current queue file
+                    fs.writeFile(
+                        'queued.json', 
+                        JSON.stringify(download_list), 
+                        () => {
+                            
+                        }
+                    );
+
                     downloadFile();
-                }, 250);
 
-            })
-            .on('progress', function(progress) {
+                })
+                .on('progress', function(progress) {
 
-                if (config.console_output) process.stdout.write("Replay ID: " + download_list[0] + " - " + progress.percent.toFixed(2) + "% completed     \r");
+                    if (config.console_output) process.stdout.write("\x1b[1;34mReplay \x1b[1;33m" + download_list[0] + " \x1b[1;34m- \x1b[1;33m" + progress.percent.toFixed(2) + "%     \r");
 
-            })
-            .on('start', function(c) {
-                downloadActive = true;
-            })
-            .on('error', function(err, stdout, exterr) {
-                if (config.console_output) process.stdout.write(download_list[0] + " - errored (Details: "+err+")     \n");
+                })
+                .on('start', function(c) {
+                    downloadActive = true;
+                })
+                .on('error', function(err, stdout, exterr) {
+                    if (config.console_output) process.stdout.write("\x1b[1;34mReplay \x1b[1;33m" + download_list[0] + " \x1b[1;34m- \x1b[1;31mErrored \x1b[1;36m(\x1b[1;34mDetails: \x1b[1;37m"+err+"\x1b[1;36m)   \n");
 
-                errored_list.push(download_list[0]);
-                download_list.shift();
-                download_active = false;
+                    errored_list.push(download_list[0]);
+                    download_list.shift();
+                    downloadActive = false;
 
-                // Update errored file
-                fs.writeFile(
-                    'errored.json', 
-                    errored_list.join("\n"),
-                    () => {
-                        // Errored file was written
+                    // Update current queue file
+                    fs.writeFile(
+                        'queued.json', 
+                        JSON.stringify(download_list), 
+                        () => {
+                            // Queue file was written
+                        }
+                    );
+
+                    // Update errored file
+                    fs.writeFile(
+                        'errored.json', 
+                        errored_list.join("\n"),
+                        () => {
+                            // Errored file was written
+                        }
+                    );
+
+                    downloadFile();
+
+                })
+                .run();
+            } else {
+                filename += '.ts';
+
+                m3u8stream(video, {
+                    chunkReadahead: config.downloadChunks,
+                    on_progress: (e) => {
+
+                        var p = Math.floor((e.index / e.total) * 10000) / 100;
+                        if (config.console_output) process.stdout.write("\x1b[1;34mReplay \x1b[1;33m" + download_list[0] + " \x1b[1;34m- \x1b[1;33m" + p + "%               \r");
+
+                    }, 
+                    on_complete: (e) => {
+                        if (config.console_output) process.stdout.write("\x1b[1;34mReplay \x1b[1;33m" + download_list[0] + " \x1b[1;34m- downloaded.                   \n");
+
+                        download_list.shift();
+                        downloadActive = false;
+
+                        // Update current queue file
+                        fs.writeFile(
+                            'queued.json', 
+                            JSON.stringify(download_list), 
+                            () => {
+                                
+                            }
+                        );
+
+                        downloadFile();
+                    },
+                    on_error: (e) => {
+
+                        // We ignore the timeout errors to avoid issues.
+                        if (e.error == 'Download timeout') return;
+
+                        if (config.console_output) process.stdout.write("\x1b[1;34mReplay \x1b[1;33m" + download_list[0] + " \x1b[1;34m- \x1b[1;31mErrored \x1b[1;36m(\x1b[1;34mDetails: \x1b[1;37m"+err+"\x1b[1;36m)   \n");
+
+                        errored_list.push(download_list[0]);
+                        download_list.shift();
+                        downloadActive = false;
+
+                        // Update current queue file
+                        fs.writeFile(
+                            'queued.json', 
+                            JSON.stringify(download_list), 
+                            () => {
+                                // Queue file was written
+                            }
+                        );
+
+                        // Update errored file
+                        fs.writeFile(
+                            'errored.json', 
+                            errored_list.join("\n"),
+                            () => {
+                                // Errored file was written
+                            }
+                        );
+
+                        downloadFile();
                     }
-                );
+                }).pipe(fs.createWriteStream(config.downloadPath + '/' + filename));
 
-                setTimeout(() => {
-                    downloadFile();
-                }, 250);
+            }
 
 
-            })
-            .run();
+
 
     });
 
